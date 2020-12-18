471e5bea88677ee6d9879d62c4500544d6ee6cf0
yaananth (Yash) <yaananth@github.com>
2020-12-18 16:13:22 -0500

    track zips

--- pushContent.bat
+++ pushContent.bat
@@ -2,6 +2,7 @@ for %%I in (.) do set CurrDirName=%%~nxI
 git init
 git remote add origin git@github.com:ORGNAME/%CurrDirName%.git
 git lfs track "*.mp4"
+git lfs track "*.zip"
 git add .gitattributes
 git add --all
 git commit -am "changes"
